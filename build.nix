740a26c2d8b6eb7b932dcf477992d3e3eb0465d7
alec <alecrodriguez1247@gmail.com>
2024-12-12 21:28:15 -0600

    idk

--- build.nix
+++ build.nix
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
     buildPhase = "cabal build" 
     installPhase = 
             "mkdir -p $out/bin" + 
-                    "cp -r ./dist-newstyle/build/adventure-game/ $out/bin/";
+            "cabal run | $out/bin/";
 
     shellHook = "echo \"Installation complete.\"";
 
