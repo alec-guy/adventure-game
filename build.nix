3aa64cc0bc29afcf6d8bfdd71467bf3f2e7f824b
alec <alecrodriguez1247@gmail.com>
2024-12-12 21:32:54 -0600

    trying best

--- build.nix
+++ build.nix
@@ -5,19 +5,11 @@
 stdenv.mkDerivation rec {
     name = "adventure-game";
     version = "1.0.0";
-    src = fetchFromGitHub {
-        owner = "alec-guy"; 
-        repo = "adventure-game"; 
-        rev  = "master"; 
-        sha256 = ""; 
-
-    };
+    src = "./adventure-game"
     buildInputs = [haskell.compiler.ghc910 haskellPackages.cabal-install haskellPackages.GLUT]; 
 
     buildPhase = "cabal build" 
-    installPhase = 
-            "mkdir -p $out/bin" + 
-            "cabal run | $out/bin/";
+    installPhase = "mkdir -p $out/bin";
 
     shellHook = "echo \"Installation complete.\"";
 
